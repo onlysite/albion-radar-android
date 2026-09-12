36f7329196a65f6e2acda2e9f2fedace9dc7bb06
onlysite <174809981+onlysite@users.noreply.github.com>
2026-09-12 13:42:31 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -7,10 +7,11 @@ const config = getDefaultConfig(projectRoot);
 const modulesRoot = path.join(projectRoot, 'modules');
 config.watchFolders = [modulesRoot];
 
-// 👇 重点：把 css 加入可识别的源码后缀
 config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json', 'css'];
-
-// 官方推荐：css不要放在assetExts里，要放在sourceExts
 config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'css');
+
+// ✅关键：开启实验性解析器，支持 package.json "exports"（解决 trpc 问题）
+config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = ['react-native', 'browser', 'require', 'import'];
 
 module.exports = config;
